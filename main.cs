d3069a78089ccffffdebdd6ca2b85e9cc0d4b4de
gustavolima1904 <119372901+gusenac@users.noreply.github.com>
2023-02-10 00:24:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+ string entrada;
+    Double m1,valor; 
+    
+    Console.WriteLine("Digite quantidade de maçãs");
+  
+    entrada = Console.ReadLine();
+
+    m1 =  Double.Parse(entrada);
+    
+if(m1>=12)
+{
+    valor = (m1*1);
+    
+      
+    Console.WriteLine ("As maçãs custam " + valor);
+
+    }
+    
+else{
+  valor = (m1*1.30);
+
+  Console.WriteLine ("As maçãs custam " + valor);
+}
+ 
+    
+  }
+
+}
